d74ac376f2726567c9d7a986475edb4a38f35c11
Ann <socevilaf@google.com>
2025-12-16 20:51:18 +0300

    Add Header

--- postcss.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: [
-    require('autoprefixer'), // Добавление автопрефиксов
-    require('cssnano'), // Минимизация CSS
-    require('postcss-preset-env'),
-    require('postcss-import'),
-  ],
-};
---
+++ src/widgets/Header/ui/Header.tsx
@@ -0,0 +1,11 @@
+import classes from './Headers.module.scss';
+
+const Headers = () => {
+  return (
+    <header>
+      <div className={classes.logo}>NewsYearHub</div>
+    </header>
+  );
+};
+
+export default Headers;
