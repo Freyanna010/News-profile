cf987faeac1d005036ac368708f9efb9c01c1634
Ann <socevilaf@google.com>
2025-12-22 13:51:14 +0300

    Fix MainLoyout

--- src/app/layouts/MainLayout/ui/MainLayout.tsx
+++ src/app/layouts/MainLayout/ui/MainLayout.tsx
@@ -1,5 +1,18 @@
+import { Outlet } from 'react-router-dom';
+
+import { Header } from '@/widgets/Header';
+
+import classes from './MainLayout.module.scss';
+
 const MainLayout = () => {
-  return <div></div>;
+  return (
+    <>
+      <Header />
+      <main className={classes.main}>
+        <Outlet />
+      </main>
+    </>
+  );
 };
 
 export default MainLayout;
