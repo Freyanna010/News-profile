ccc22c06d685498201970471c286ab1a8991168a
Ann <socevilaf@google.com>
2025-12-22 14:08:41 +0300

    Fix styles in Header

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -8,26 +8,28 @@ import classes from './Header.module.scss';
 const Header = () => {
   return (
     <header className={classes.header}>
-      <div className={classes.leftFlex}>
-        <div className={classes.logo}>
-          <img src={logo} />
+      <div className={classes.inner}>
+        <div className={classes.leftFlex}>
+          <div className={classes.logo}>
+            <img src={logo} alt="logo" />
+          </div>
+          <div className={classes.title}>
+            <h1>NewsYeaHub</h1>
+          </div>
         </div>
-        <div className={classes.title}>
-          <h1>NewsYeaHub</h1>
-        </div>
-      </div>
 
-      <NavBar type="horizontal">
-        <AppLink to="/news" type="primary">
-          <p>новости</p>
-        </AppLink>
-        <AppLink to="/news" type="primary">
-          <p>новости</p>
-        </AppLink>
-        <AppLink to="/news" type="primary">
-          <p>новости</p>
-        </AppLink>
-      </NavBar>
+        <NavBar type="horizontal">
+          <AppLink to="/news" type="primary">
+            новости
+          </AppLink>
+          <AppLink to="/news" type="primary">
+            новости
+          </AppLink>
+          <AppLink to="/news" type="primary">
+            новости
+          </AppLink>
+        </NavBar>
+      </div>
     </header>
   );
 };
