36fe7969f27fc1ead613bbe5a7ae96e612a92b27
Ann <socevilaf@google.com>
2025-12-22 17:59:18 +0300

    Fixs links in Navb

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -2,6 +2,7 @@ import logo from '@shared/assets/logo.avif';
 
 import { NavBar } from '@/shared/ui/NavBar';
 import { AppLink } from '@/shared/ui/AppLink';
+import { RoutePath } from '@/shared/config/routeConfig/routeConfig';
 
 import classes from './Header.module.scss';
 
@@ -19,14 +20,18 @@ const Header = () => {
         </div>
 
         <NavBar type="horizontal" align="end" spacing="small">
-          <AppLink to="/news" type="primary">
-            новости
+          <AppLink to={RoutePath.news} type="primary">
+            <p>новости</p>
           </AppLink>
-          <AppLink to="/news" type="primary">
-            новости
+          <AppLink to={RoutePath.main} type="primary">
+            <p>главная</p>
           </AppLink>
-          <AppLink to="/news" type="primary">
-            новости
+          <AppLink to={RoutePath.profile} type="primary">
+            <p>профиль</p>
+          </AppLink>
+          <AppLink to={RoutePath.login} type="primary">
+            //TODO: добавить "выйти"
+            <p>войти</p>
           </AppLink>
         </NavBar>
       </div>
