7bd8ffc70e82076bb859b4a9ada4e711955eafdc
Ann <socevilaf@google.com>
2025-12-22 17:47:17 +0300

    Add ProtectedRoute

---
+++ src/app/providers/router/ProtectedRout.tsx
@@ -0,0 +1,22 @@
+import { Navigate, useLocation } from 'react-router-dom';
+import type { ReactNode } from 'react';
+
+import { useMockAuth } from '@/shared/lib/hooks/useMockAuth';
+import { RoutePath } from '@/shared/config/routeConfig/routeConfig';
+
+interface ProtectedRouteProps {
+  children: ReactNode;
+}
+const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
+  const { isAuthenticated } = useMockAuth();
+
+  const location = useLocation();
+
+  if (!isAuthenticated) {
+    return <Navigate to={RoutePath.login} replace state={{ from: location }} />;
+  }
+
+  return <>{children}</>;
+};
+
+export default ProtectedRoute;
