8d084d8e72d2806d6861bad0757d6d105588ee7f
Ann <socevilaf@google.com>
2025-12-23 12:16:05 +0300

    Add router config

---
+++ src/app/providers/router/roterConfig.tsx
@@ -0,0 +1,70 @@
+import { Suspense } from 'react';
+import { MoonLoader } from 'react-spinners';
+import { createBrowserRouter } from 'react-router-dom';
+
+import { AuthLayout } from '@/app/layouts/AuthLayout';
+import { MainLayout } from '@/app/layouts/MainLayout';
+import { HomePage } from '@/pages/HomePage';
+import { LoginPage } from '@/pages/LoginPage';
+import { NewsPage } from '@/pages/NewsPage';
+import { NotFoundPage } from '@/pages/NotFoundPage';
+import { ProfilePage } from '@/pages/ProfilePage';
+import { RoutePath } from '@/shared/constans/route';
+
+import ProtectedRoute from './ProtectedRout';
+
+export const router = createBrowserRouter(
+  [
+    {
+      element: (
+        <Suspense fallback={<MoonLoader color="#5a17ff" size={90} />}>
+          <MainLayout />
+        </Suspense>
+      ),
+      children: [
+        {
+          path: RoutePath.main,
+          element: <HomePage />,
+        },
+        {
+          path: RoutePath.news,
+          element: <NewsPage />,
+        },
+        {
+          path: RoutePath.profile,
+          element: (
+            <ProtectedRoute>
+              <ProfilePage />
+            </ProtectedRoute>
+          ),
+        },
+      ],
+    },
+
+    {
+      element: (
+        <Suspense fallback={<MoonLoader color="#5a17ff" size={90} />}>
+          <AuthLayout />
+        </Suspense>
+      ),
+      children: [
+        {
+          path: RoutePath.login,
+          element: <LoginPage />,
+        },
+      ],
+    },
+
+    {
+      path: RoutePath.not_found,
+      element: (
+        <Suspense fallback={<MoonLoader color="#ff1717ff" size={90} />}>
+          <NotFoundPage />
+        </Suspense>
+      ),
+    },
+  ],
+  {
+    basename: '/NewsYeaHub',
+  }
+);
