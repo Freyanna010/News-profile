023db539f5217fbd9095924e421d26337cdfbe95
Ann <socevilaf@google.com>
2026-01-06 13:50:07 +0300

    Add StoreProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from 'react-dom/client';
+import { Provider as StoreProvider } from 'react-redux';
 
 import App from './app/App.tsx';
+import { store } from './app/providers/store/storeConfig.ts';
 
-createRoot(document.getElementById('root')!).render(<App />);
+createRoot(document.getElementById('root')!).render(
+  <StoreProvider store={store}>
+    <App />
+  </StoreProvider>
+);
