7df470f21bc1517ad890598db858847127498e6a
Ann <socevilaf@google.com>
2026-01-06 17:39:19 +0300

    Mapping news in NewsPage

--- src/pages/NewsPage/ui/NewsPage.tsx
+++ src/pages/NewsPage/ui/NewsPage.tsx
@@ -1,8 +1,22 @@
+import { MoonLoader } from 'react-spinners';
+
+import { useGetNewsQuery } from '@/features/news-crud/model/api/newsApi';
+
 const NewsPage = () => {
+  const { data: news, isLoading, error } = useGetNewsQuery();
+
+  if (isLoading) return <MoonLoader color="#5a17ff" size={90} />;
+  if (error) return <div>Ошибка: {JSON.stringify(error)}</div>;
+
   return (
     <div>
       <h1>Новости</h1>
-      <p>Здесь будут последние новости...</p>
+      {news?.map((item) => (
+        <div key={item.id} style={{ marginBottom: '20px' }}>
+          <h3>{item.title}</h3>
+          <p>{item.body}</p>
+        </div>
+      ))}
     </div>
   );
 };
