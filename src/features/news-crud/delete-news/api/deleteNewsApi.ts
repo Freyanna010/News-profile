78e7506143f2638695bf25c7187258c4478ea138
Ann <socevilaf@google.com>
2026-01-16 16:49:39 +0300

    create deleteNewsApi

---
+++ src/features/news-crud/delete-news/api/deleteNewsApi.ts
@@ -0,0 +1,17 @@
+import { baseApi } from '@/shared/config/query';
+import type { News } from '@/entities/news';
+import { API_TAGS } from '@/shared/constans';
+
+export const deleteNewsApi = baseApi.injectEndpoints({
+  endpoints: (build) => ({
+    deleteNews: build.mutation<void, News['id']>({
+      query: (newsId) => ({
+        url: `/posts/${newsId}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: [API_TAGS.NEWS],
+    }),
+  }),
+});
+
+export const { useDeleteNewsMutation } = deleteNewsApi;
---
+++ src/features/news-crud/delete-news/index.ts
@@ -0,0 +1,1 @@
+export { useDeleteNewsMutation } from './api/deleteNewsApi';
